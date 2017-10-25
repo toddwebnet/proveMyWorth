28badd5b23e0478c47ca8c7a48d95f0052f57149
James Todd <james.todd@printerlogic.com>
2017-10-25 15:08:04 -0500

    add user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (User::all()->count() == 0) {
+            User::create([
+                'name' => 'System Administrator',
+                'email' => env("ADMIN_EMAIL"),
+                'password' => bcrypt('password1'),
+            ]);
+        }
+    }
+}
