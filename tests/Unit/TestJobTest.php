170310db2b13d74f03bf5c9e8bcce5aa40c96dea
James Todd <james.todd@printerlogic.com>
2017-10-25 16:14:03 -0500

    add test job unit test

--- tests/Unit/TestJobTest.php
+++ tests/Unit/TestJobTest.php
@@ -1,15 +1,28 @@
 <?php
+
 namespace Tests\Unit;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\TestJob;
+use App\Services\TestJobService;
 use Tests\AppTestCase;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class TestJobTest extends AppTestCase
 {
-    public function testOne()
+    public function testTestJob()
     {
+        $numJobsBefore = $this->getTestJobsCount();
 
+        TestJobService::workTestJob();
+
+        $numJobsAfter = $this->getTestJobsCount();
+
+        // assumes there is 1 new entry into the test_jobs table
+        $this->assertEquals(($numJobsAfter-$numJobsBefore), 1);
+    }
+
+    private function getTestJobsCount()
+    {
+        return TestJob::all()->count();
     }
 }
