4fba97081a6f576afc40dcbe664f331bd2b3b1e4
James Todd <james.todd@printerlogic.com>
2017-10-27 11:28:45 -0500

    js validation

--- public/js/application.js
+++ public/js/application.js
@@ -36,7 +36,45 @@ function placeSpinner() {
     $('#mainContent').html(spinnerHTML);
 }
 
+function validateSaveProfile() {
+
+    var errs = [];
+    if ($("#profileForm input[name='name']").val() == "") {
+        errs.push('Name is required');
+    }
+    if ($("#profileForm input[name='email']").val() == "") {
+        errs.push('Email is required');
+    }
+    if ($("#profileForm input[name='birthdate']").val() == "") {
+        errs.push('DOB is required');
+    }
+    if ($("#profileForm input[name='phone_number']").val() == "") {
+        errs.push('Phone is required');
+    }
+    if ($("#profileForm input[name='address']").val() == "") {
+        errs.push('Address is required');
+    }
+    if ($("#profileForm input[name='city']").val() == "") {
+        errs.push('City is required');
+    }
+    if ($("#profileForm input[name='state']").val() == "") {
+        errs.push('State is required');
+    }
+    if ($("#profileForm input[name='zip']").val() == "") {
+        errs.push('Zipcode is required');
+    }
+
+    if (errs.length > 0) {
+        alert(errs.join('\n'));
+        return false;
+    }
+    return true;
+}
+
 function saveProfile() {
+    if (!validateSaveProfile()) {
+        return false;
+    }
     formData = $('#profileForm').serialize();
     url = '/ajax/profile';
     $.ajax({
